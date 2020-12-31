b5bc545fde8ffd50a80de70cf708847571217763
pragya.srivastava3888@gmail.com <49591103+pragya2404@users.noreply.github.com>
2020-12-31 14:43:00 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const router = require('./router/router')   //Contains all router functions
+
+const app = express()
+app.use(express.json())
+app.use(router)
+
+//Handling error when the POST JSON has a bad format
+app.use(function (error, req, res, next) {
+    if(error instanceof SyntaxError){
+        res.send("Error: Bad JSON format")
+    } else {
+      next();
+    }
+});
+
+app.listen(3000)
